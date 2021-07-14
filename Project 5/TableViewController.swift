888405c91723bdeb8cfedfdad0abc94a39cec5be
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-07-14 12:34:03 -0300

    Setting Up

---
+++ Project 5/TableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewController.swift
+//  Project 5
+//
+//  Created by Henrique Silva on 14/07/21.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
